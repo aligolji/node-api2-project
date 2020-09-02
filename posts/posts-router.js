f6cae348d45c333c3604a84372deeeea4100a141
aligolji <aligolji@users.noreply.github.com>
2020-09-02 16:33:02 -0700

    making progress with CRUD requests

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/', (req, res) => {
     const { title, contents } = req.body;
     const newPost = { title, contents };
+
     if (!title || !contents) {
         res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
 
@@ -20,11 +21,27 @@ router.post('/', (req, res) => {
     }
 });
 
-
-
-
 router.post('/:id/comments', (req, res) => {
-    res.status(200).json({ hello: 'posts' })
+    const { id } = req.params;
+    const { text } = req.body;
+    const newComment = { text };
+    const findPostByID = db.findById = id => {
+        return post_id == id;
+    };    
+
+    if (!findPostByID) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+
+    } else if (!text) {
+        res.status(400).json({ errorMessage: 'Please provide text for the comment.' });
+
+    } else if (text) {
+        db.insertComment(newComment)
+        res.status(201).json(newComment);
+
+    } else if (error) {
+        res.status(500).json({ error: 'There was an error while saving the comment to the database.' });
+    }
 });
 
 
